761fbd1729bef91954fe7ab91ec5ccb1b30d9704
mvdwoord <maarten@vanderwoord.nl>
2017-01-17 18:08:13 +0100

    Added initial support for parameters and tasks

--- bbreport.py
+++ bbreport.py
@@ -43,6 +43,11 @@ __status__ = "Development"
 # ToDo parameterize output folder
 output_folder = "./output"
 
+# Some lookup dicts for RES magic numbers
+parameter_type = {
+    '0': 'Text'
+}
+
 
 def bbprocess(bb):
     """Open Building Block file, parse as xml and dispatch thes sections to their respective parser functions"""
@@ -54,25 +59,69 @@ def bbprocess(bb):
             os.makedirs(output_folder)
             bbtree = (etree.parse(buildingblock))
             moduleroot = bbtree.find("/buildingblock/modules")
-            print(moduleroot, len(moduleroot))
-            for element in moduleroot.getchildren():
-                create_module_page(element)
+            if len(moduleroot) > 0:
+                os.makedirs(output_folder + '/modules')
+                for element in moduleroot.getchildren():
+                    create_module_page(element)
 
     except IOError as err:
         print("Error opening {}\n{}".format(bb, err))
 
 
+def parameter_to_dict(p):
+    """Retruns a dictionary from a param xml element"""
+    value_element = p.find('.//value1')
+    value = ""
+    if value_element is not None:
+        value = value_element.text
+    parameterdict = {
+        'name': (p.find('.//name')).text,
+        'type': parameter_type.get(p.find('.//type').text, "Unknown"),
+        'value': value,
+        'description': (p.find('.//description')).text
+    }
+    return parameterdict
+
+
+def task_to_dict(t):
+    type = t.find('.//properties/type').text
+    taskdict = {
+        'type': type,
+        'guid': t.find('.//properties/guid').text,
+        'enabled': t.find('.//properties/enabled').text
+    }
+    if type == "PWRSHELL":
+        pass
+
+    return taskdict
+
+
 def create_module_page(e):
     """Creates a html from jinja template and a module element"""
-    # print("creating html for element {}".format(e.tag))
+    folderpath = '/'.join([f.text for f in e.findall('.//folder/name')])
+    paramroot = e.find('.//tasks/task/parameters')
+    # we need to grab the non hidden tasks, thanks RES
+    actual_tasks = e.xpath('.//tasks/task[not(@hidden)]')
     module = {
         'title': (e.find('.//name')).text,
-        'guid': (e.find('.//guid')).text
+        'guid': (e.find('.//guid')).text,
+        'enabled': (e.find('.//enabled')).text,
+        'description': (e.find('.//description')).text,
+        'version': (e.find('.//version')).text,
+        'versioncomment': (e.find('.//versioncomment')).text,
+        'folderpath': folderpath,
+        'parameters': [],
+        'tasks': []
     }
+    if len(paramroot) > 0:
+        for element in paramroot.getchildren():
+            module['parameters'].append(parameter_to_dict(element))
+    for task in actual_tasks:
+        module['tasks'].append(task_to_dict(task))
     # print(module['title'], module['guid'])
     template = env.get_template('module.html')
     html = template.render(module=module)
-    filename = output_folder + "/module_" + module['guid'] + ".html"
+    filename = output_folder + "/modules/" + module['guid'] + ".html"
     with open(filename, 'wt', encoding='utf-8') as file:
         file.write(html)
 
